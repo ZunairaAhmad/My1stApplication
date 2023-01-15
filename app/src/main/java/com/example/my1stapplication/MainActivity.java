5088c7115c838f144079ee26fd12e42a1da93c22
ZunairaAhmad <bitf19a016@pucit.edu.pk>
2023-01-14 18:02:05 -0800

    pic and colors addition

--- app/src/main/java/com/example/my1stapplication/MainActivity.java
+++ app/src/main/java/com/example/my1stapplication/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.my1stapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnPrime;
+    Button btnComp;
+
+    TextView textQ;
+    TextView textAns;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void RandomNumbersGeneration(){
+
+    }
+
+    @Override
+    public void onClick(View view){
+
     }
 }
