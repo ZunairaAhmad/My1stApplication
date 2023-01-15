50d426e7fc374e52d0679f589b41fd244ea0accb
ZunairaAhmad <bitf19a016@pucit.edu.pk>
2023-01-14 23:57:06 -0800

    UI changes and implimentation of random number generating function

--- app/src/main/java/com/example/my1stapplication/MainActivity.java
+++ app/src/main/java/com/example/my1stapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -15,14 +16,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     TextView textQ;
     TextView textAns;
 
+    int num=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textQ=findViewById(R.id.textQuestions);
     }
 
     private void RandomNumbersGeneration(){
 
+        Random randomNo= new Random();
+        num=randomNo.nextInt(100)+1;
+
+        boolean flag = false;
+        for (int i = 2; i <= num/ 2; ++i) {
+            // condition for nonprime number
+            if (num % i == 0) {
+                flag = true;
+                break;
+            }
+        }
     }
 
     @Override
