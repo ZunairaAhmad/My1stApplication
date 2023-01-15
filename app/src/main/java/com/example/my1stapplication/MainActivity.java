a3bfe70658ccb015e8e5216b40884310651e8878
ZunairaAhmad <bitf19a016@pucit.edu.pk>
2023-01-15 01:39:07 -0800

    logic implimentation and add more Text Views

--- app/src/main/java/com/example/my1stapplication/MainActivity.java
+++ app/src/main/java/com/example/my1stapplication/MainActivity.java
@@ -16,26 +16,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     TextView textQ;
     TextView textAns;
 
+    int questCount;
     int num=0;
+    boolean flag = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textQ=findViewById(R.id.textQuestions);
+        btnPrime = findViewById(R.id.prime);
+        btnPrime.setOnClickListener(this);
+        btnComp = findViewById(R.id.composite);
+        btnComp.setOnClickListener(this);
+
+        RandomNumbersGeneration();
     }
 
     private void RandomNumbersGeneration(){
 
         Random randomNo= new Random();
-        num=randomNo.nextInt(100)+1;
 
-        boolean flag = false;
-        for (int i = 2; i <= num/ 2; ++i) {
-            // condition for nonprime number
-            if (num % i == 0) {
-                flag = true;
-                break;
+        if(questCount<=20) {
+            num = randomNo.nextInt(100) + 1;
+            textQ.setText("Question No: "+questCount);
+            textQ.setText(num + " is Prime or Composite?");
+
+            for (int i = 2; i <= num / 2; ++i) {
+                // condition for nonprime number
+                if (num % i == 0) {
+                    flag = true;
+                    break;
+                }
             }
         }
     }
